3d74018d8c99378ff3dcdf632a5bc8c4c6ac1276
Mambodiev <68683710+Mambodiev@users.noreply.github.com>
2023-12-21 03:14:51 +0300

    collectstatic

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,15 +1,8 @@
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise import WhiteNoise
 
+from i import MyWSGIApp
 
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
-application = get_wsgi_application()
-
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = MyWSGIApp()
+application = WhiteNoise(application, root="/path/to/static/files")
+application.add_files("/path/to/more/static/files", prefix="more-files/")
 
