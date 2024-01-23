f730972be059452531d053c5cf47c56cbf7ff587
Mambodiev <68683710+Mambodiev@users.noreply.github.com>
2024-01-23 17:42:12 +0300

    collectstatic

--- config/settings.py
+++ config/settings.py
@@ -93,17 +93,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.sqlite3',
-#          'NAME': BASE_DIR / 'db.sqlite3',
-#      }
-#  }
-
-
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
+
+
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
